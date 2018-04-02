183c5ec7ea24ce0921194af8d3a76baf64d2210a
Ihor <eldaeron@mail.ua>
2018-04-02 15:35:18 +0300

    Update ecommerce.php

--- ecommerce.php
+++ ecommerce.php
@@ -1,9 +1,10 @@
 <?php
-  /**
-	* Enhanced Ecommerce Payloads
-    * @link https://developers.google.com/analytics/devguides/collection/protocol/v1/devguide?hl=ru (Docs)
-    * @link https://github.com/Eldaeron/php-ga-tools (Script)
-	*/
+/**
+ * Enhanced Ecommerce Payloads
+ * @link https://developers.google.com/analytics/devguides/collection/protocol/v1/devguide?hl=ru (Docs)
+ * @link https://github.com/Eldaeron/php-ga-tools (Script)
+ *
+ */
 
 namespace PhpGaTools;
 
@@ -13,8 +14,8 @@ class EnhancedEcommerce
 		$transaction = array(
 			'pa' => 'purchase',		// Product action (purchase). Required.
 			'ti' => $orderId,		// Transaction ID. Required.
-			'ta' => $affiliation,	// Transaction Affiliation Example "Google Store Online"
-			'tt' => $tax,			// Transaction tax.
+			'ta' => $affiliation,		// Transaction Affiliation Example "Google Store Online"
+			'tt' => $tax,		// Transaction tax.
 			'ts' => $shipping,		// Transaction shipping.
 			'tr' => $revenue,		// Revenue
 			'tcc' => $coupon		// Transaction coupon
@@ -22,13 +23,13 @@ class EnhancedEcommerce
 
 		for ($i=0; $i < count($products); $i++) {
 			$n = $i+1;
-			$transaction["pr{$n}id"] = $products[$i]['id'];				// Product 1 ID. Either ID or name must be set.
-			$transaction["pr{$n}nm"] = $products[$i]['name'];			// Product 1 name. Either ID or name must be set.
-			$transaction["pr{$n}pr"] = $products[$i]['price'];			// Product 1 Price.
-			$transaction["pr{$n}qt"] = $products[$i]['qty'];			// Product 1 quantity.
+			$transaction["pr{$n}id"] = $products[$i]['id'];		// Product 1 ID. Either ID or name must be set.
+			$transaction["pr{$n}nm"] = $products[$i]['name'];		// Product 1 name. Either ID or name must be set.
+			$transaction["pr{$n}pr"] = $products[$i]['price'];		// Product 1 Price.
+			$transaction["pr{$n}qt"] = $products[$i]['qty'];		// Product 1 quantity.
 
 			if (isset($products[$i]['category'])) {
-				$transaction["pr{$n}ca"] = $products[$i]['category'];	// Product 1 category.
+				$transaction["pr{$n}ca"] = $products[$i]['category'];		// Product 1 category.
 			} else {
 				$transaction["pr{$n}ca"] = false;
 			}
